591d9077ee530b37879bfd13d35e96f9b450ee04
Ajay Srivastava <ajaysriv3@gmail.com>
2016-06-13 11:45:18 -0400

    a little cleanup

--- 1_minute_left_lane/counter/count.cpp
+++ 1_minute_left_lane/counter/count.cpp
@@ -1,5 +1,5 @@
-#include <cv.h>
 #include <cxcore.h>
+#include <cv.h>
 #include <fstream>
 #include <highgui.h>
 #include <iostream>
@@ -9,18 +9,17 @@
 #include <vector>
 
 extern "C" {
-	#include "sift.h"
 	#include "imgfeatures.h"
 	#include "kdtree.h"
+	#include "sift.h"
 	#include "utils.h"
 	#include "xform.h"
 }
 
-
-/* the maximum number of keypoint NN candidates to check during BBF search */
+// the maximum number of keypoint NN candidates to check during BBF search
 #define KDTREE_BBF_MAX_NN_CHKS 200
 
-/* threshold on squared ratio of distances between NN and 2nd NN */
+// threshold on squared ratio of distances between NN and 2nd NN
 #define NN_SQ_DIST_RATIO_THR 0.49
 
 #define MINIMUM_FRAMES 3
@@ -49,25 +48,15 @@ int main(int argc, char** argv)
 	int frame_number = START_FRAME;
 	// int final_index = END_FRAME;
 
-	// char image_file[100];
-	// sprintf(image_file, "../raw_frames/frame0%d.jpeg", image_index);
-	// image = imread(image_file, CV_LOAD_IMAGE_COLOR);
-
-	// cv::Mat image_mat(image);
-
-	// char frame_name[100];
-	// sprintf(frame_name, "Orignal Frame: %s", image_file);
-
-	// namedWindow(frame_name, WINDOW_AUTOSIZE);
-	// imshow(frame_name, image);
-
 	// Read bounding box data
 	ifstream bbox_file("../bbox_files/car_boxes.txt");
 
+	// Vector of vectors of vectors for bbox data
 	vector<vector<vector<float>>> bboxes(END_FRAME - START_FRAME);
 
 	float xmin, ymin, xmax, ymax;
 
+	// Read bbox data from file
 	do
 	{
 		// Check if frame number in bbox file matches frame index
@@ -89,13 +78,6 @@ int main(int argc, char** argv)
 			bboxes[frame_number - START_FRAME].push_back(current_bbox);
 
 			// printf("frame: %d\txmin: %f\tymin: %f\txmax: %f\tymax: %f\t\n", image_index, xmin, ymin, xmax, ymax);
-
-			// cv::Rect bound_box((int)xmin, (int)ymin, (int)(xmax-xmin), (int)(ymax-ymin));
-			// cv::Mat cropped = image_mat(bound_box);
-
-			// namedWindow( "Display window2", WINDOW_NORMAL );//mak Create a window for display.
-			// imshow(to_string(b), cropped );                   // Show our image inside it.
-
 		}
 		else
 		{
@@ -105,7 +87,7 @@ int main(int argc, char** argv)
 
 	bbox_file.close();
 
-	// Sort all bbox vectors
+	// Sort all bbox vectors by leftmost x coordinate
 	for (int f=START_FRAME; f<END_FRAME; f++)
 	{
 		std::sort(bboxes[f-START_FRAME].begin(), bboxes[f-START_FRAME].end(),
@@ -115,7 +97,8 @@ int main(int argc, char** argv)
 			});
 	}
 
-	for (int f=START_FRAME; f<END_FRAME; f++)
+	// Print out bbox data for confirmation
+	/*for (int f=START_FRAME; f<END_FRAME; f++)
 	{
 		int i = 0;
 		for (auto bbox : bboxes[f-START_FRAME])
@@ -123,7 +106,7 @@ int main(int argc, char** argv)
 			i++;
 			printf("Frame: %d\tbbox: %d\txmin: %f\tymin %f\txmax: %f\tymax %f\n", f, i, bbox[0], bbox[1], bbox[2], bbox[3]);
 		}
-	}
+	} */
 
 	int matched_frames = 0;
 	int car_count = 0;
@@ -139,9 +122,9 @@ int main(int argc, char** argv)
 			continue;
 		}
 		cv::Rect bound_box_zero(bboxes[f-START_FRAME-1][0][0],
-							bboxes[f-START_FRAME-1][0][1],
-							bboxes[f-START_FRAME-1][0][2] - bboxes[f-START_FRAME-1][0][0],
-							bboxes[f-START_FRAME-1][0][3] - bboxes[f-START_FRAME-1][0][1]);
+								bboxes[f-START_FRAME-1][0][1],
+								bboxes[f-START_FRAME-1][0][2] - bboxes[f-START_FRAME-1][0][0],
+								bboxes[f-START_FRAME-1][0][3] - bboxes[f-START_FRAME-1][0][1]);
 		cv::Mat cropped_zero = image_zero(bound_box_zero);
 		IplImage img1_image = cropped_zero;
 		IplImage* img1 = &img1_image;
@@ -155,16 +138,12 @@ int main(int argc, char** argv)
 			continue;
 		}
 		cv::Rect bound_box_one(bboxes[f-START_FRAME][0][0],
-							bboxes[f-START_FRAME][0][1],
-							bboxes[f-START_FRAME][0][2] - bboxes[f-START_FRAME][0][0],
-							bboxes[f-START_FRAME][0][3] - bboxes[f-START_FRAME][0][1]);
+								bboxes[f-START_FRAME][0][1],
+								bboxes[f-START_FRAME][0][2] - bboxes[f-START_FRAME][0][0],
+								bboxes[f-START_FRAME][0][3] - bboxes[f-START_FRAME][0][1]);
 		cv::Mat cropped_one = image_one(bound_box_one);
 		IplImage img2_image = cropped_one;
 		IplImage* img2 = &img2_image;
-
-
-		//compare
-
 
 		IplImage *stacked;
 		struct feature* feat1, * feat2, * feat;
@@ -177,38 +156,38 @@ int main(int argc, char** argv)
 		stacked = stack_imgs(img1, img2);
 
 		// fprintf( stderr, "Finding features in %s...\n", "CROPPED ONE");
-		n1 = sift_features( img1, &feat1 );
+		n1 = sift_features(img1, &feat1);
 		// fprintf( stderr, "Finding features in %s...\n", "CROPPED TWO" );
-		n2 = sift_features( img2, &feat2 );
+		n2 = sift_features(img2, &feat2);
 		// fprintf( stderr, "Building kd tree...\n" );
-		kd_root = kdtree_build( feat2, n2 );
-		for( i = 0; i < n1; i++ )
+		kd_root = kdtree_build(feat2, n2);
+		for(i = 0; i < n1; i++)
 			{
 				feat = feat1 + i;
-				k = kdtree_bbf_knn( kd_root, feat, 2, &nbrs, KDTREE_BBF_MAX_NN_CHKS );
-				if( k == 2 )
+				k = kdtree_bbf_knn(kd_root, feat, 2, &nbrs, KDTREE_BBF_MAX_NN_CHKS);
+				if(k == 2)
 				{
-					d0 = descr_dist_sq( feat, nbrs[0] );
-					d1 = descr_dist_sq( feat, nbrs[1] );
-					if( d0 < d1 * NN_SQ_DIST_RATIO_THR )
+					d0 = descr_dist_sq(feat, nbrs[0]);
+					d1 = descr_dist_sq(feat, nbrs[1]);
+					if(d0 < d1 * NN_SQ_DIST_RATIO_THR)
 						{
-							pt1 = cvPoint( cvRound( feat->x ), cvRound( feat->y ) );
-							pt2 = cvPoint( cvRound( nbrs[0]->x ), cvRound( nbrs[0]->y ) );
+							pt1 = cvPoint(cvRound(feat->x), cvRound(feat->y));
+							pt2 = cvPoint(cvRound(nbrs[0]->x), cvRound(nbrs[0]->y));
 							pt2.y += img1->height;
-							cvLine( stacked, pt1, pt2, CV_RGB(255,0,255), 1, 8, 0 );
+							cvLine(stacked, pt1, pt2, CV_RGB(255, 0, 255), 1, 8, 0);
 							m++;
 							feat1[i].fwd_match = nbrs[0];
 						}
 				}
-				free( nbrs );
+				free(nbrs);
 			}
 
 		// fprintf(stderr, "Found %d total matches\n", m);
 
-		char *matches = "Matches";
-
-		display_big_img( stacked, matches );
-		cvWaitKey(1);
+		// Uncomment to display bboxes and matches
+		// char *matches = "Matches";
+		// display_big_img(stacked, matches);
+		// cvWaitKey(1);
 
 		printf("%d\n", f);
 
@@ -254,13 +233,13 @@ int main(int argc, char** argv)
 					return 1;
 				}
 
-				cvWaitKey(0);
+				// Uncomment to wait for keypress when match is detected
+				// cvWaitKey(0);
 			}
 			else if (matched_frames == MINIMUM_FRAMES)
 			{
 				car_count--;
 			}
-			// matched_frames = 0;
 		}
 	}
 	return 0;
