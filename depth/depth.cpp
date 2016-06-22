c3a7dd2ba7044ae9153520dd3a53edf5f020629d
Ajay Srivastava <ajaysriv3@gmail.com>
2016-06-22 11:54:11 -0400

    latest changes for depth

--- depth/depth.cpp
+++ depth/depth.cpp
@@ -8,6 +8,14 @@
 #include <stdio.h>
 #include <vector>
 
+#include "opencv2/calib3d/calib3d.hpp"
+// #include <opencv2/imgcodecs.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/contrib/contrib.hpp"
+
 extern "C"
 {
 	#include "imgfeatures.h"
@@ -29,7 +37,7 @@ extern "C"
 #define YMAX_THRES 650
 
 #define START_FRAME 192
-#define END_FRAME 315
+#define END_FRAME 314
 
 #define BBOX_FILE "../data/speed_tracking/bbox_files/car_45.txt"
 #define RAW_FRAMES_LOCATION "../data/speed_tracking/raw_frames/45"
@@ -46,6 +54,19 @@ void peekline(ifstream &is, string &s)
 	is.seekg(sp);
 }
 
+// static void drawOptFlowMap(const Mat& flow, Mat& cflowmap, int step, double, const Scalar& color)
+// {
+// 	for(int y = 0; y < cflowmap.rows; y += step)
+// 	{
+// 		for(int x = 0; x < cflowmap.cols; x += step)
+// 		{
+// 			const Point2f& fxy = flow.at<Point2f>(y, x);
+// 			line(cflowmap, Point(x,y), Point(cvRound(x+fxy.x), cvRound(y+fxy.y)), color);
+// 			circle(cflowmap, Point(x,y), 2, color, -1);
+// 		}
+// 	}
+// }
+
 int main(int argc, char** argv)
 {
 	Mat image;
@@ -60,6 +81,9 @@ int main(int argc, char** argv)
 	vector<vector<vector<float>>> bboxes(END_FRAME - START_FRAME);
 
 	float xmin, ymin, xmax, ymax;
+
+	vector<double> flux(END_FRAME - START_FRAME);
+	vector<double> bbox_heights(END_FRAME - START_FRAME);
 
 	// Read bbox data from file
 	do
@@ -116,47 +140,132 @@ int main(int argc, char** argv)
 	int matched_frames = 0;
 	int car_count = 0;
 
+	Mat flow, cflow, frame;
+	Mat gray, prevgray, uflow;
+
 	for (long int f=START_FRAME+1; f<END_FRAME; f++)
 	{
 		char image_file_zero[100];
 		sprintf(image_file_zero, RAW_FRAMES_LOCATION"/frame%05ld.jpeg", f-1);
 
-		printf("hey%s\n", image_file_zero);
+		// printf("%s\n", image_file_zero);
 		Mat image_zero = imread(image_file_zero, CV_LOAD_IMAGE_COLOR);
 
-		imshow("test", image_zero);
-		cvWaitKey(0);
-
-		if(bboxes[START_FRAME-1].empty())
+		if(bboxes[f-START_FRAME-1].empty())
 		{
 			continue;
 		}
-		cv::Rect bound_box_zero(bboxes[START_FRAME-1][0][0],
-								bboxes[START_FRAME-1][0][1],
-								bboxes[START_FRAME-1][0][2] - bboxes[START_FRAME-1][0][0],
-								bboxes[START_FRAME-1][0][3] - bboxes[START_FRAME-1][0][1]);
+		cv::Rect bound_box_zero(bboxes[f-START_FRAME-1][0][0],
+								bboxes[f-START_FRAME-1][0][1],
+								bboxes[f-START_FRAME-1][0][2] - bboxes[f-START_FRAME-1][0][0],
+								bboxes[f-START_FRAME-1][0][3] - bboxes[f-START_FRAME-1][0][1]);
 		cv::Mat cropped_zero = image_zero(bound_box_zero);
 		IplImage img1_image = cropped_zero;
 		IplImage* img1 = &img1_image;
 
-		puts("here\n");
 
 
 		char image_file_one[100];
-		sprintf(image_file_one, RAW_FRAMES_LOCATION"/raw_frames/frame%05ld.jpeg", f);
+		sprintf(image_file_one, RAW_FRAMES_LOCATION"/frame%05ld.jpeg", f);
 		Mat image_one = imread(image_file_one, CV_LOAD_IMAGE_COLOR);
 
-		if(bboxes[START_FRAME].empty())
+		if(bboxes[f-START_FRAME].empty())
 		{
 			continue;
 		}
-		cv::Rect bound_box_one(bboxes[START_FRAME][0][0],
-								bboxes[START_FRAME][0][1],
-								bboxes[START_FRAME][0][2] - bboxes[START_FRAME][0][0],
-								bboxes[START_FRAME][0][3] - bboxes[START_FRAME][0][1]);
+		cv::Rect bound_box_one(bboxes[f-START_FRAME][0][0],
+								bboxes[f-START_FRAME][0][1],
+								bboxes[f-START_FRAME][0][2] - bboxes[f-START_FRAME][0][0],
+								bboxes[f-START_FRAME][0][3] - bboxes[f-START_FRAME][0][1]);
 		cv::Mat cropped_one = image_one(bound_box_one);
 		IplImage img2_image = cropped_one;
 		IplImage* img2 = &img2_image;
+
+
+
+
+		// //-- 2. Call the constructor for StereoBM
+		// int ndisparities = 16*5;   /**< Range of disparity */
+		// int SADWindowSize = 21; /**< Size of the block window. Must be odd */
+
+		// Ptr<StereoBM> sbm = StereoBM::create( ndisparities, SADWindowSize );
+
+		// //-- 3. Calculate the disparity image
+		// sbm->compute( image_zero, image_one, imgDisparity16S );
+
+		// //-- Check its extreme values
+		// double minVal; double maxVal;
+
+		// minMaxLoc( imgDisparity16S, &minVal, &maxVal );
+
+		// printf("Min disp: %f Max value: %f \n", minVal, maxVal);
+
+		// Mat imgDisparity16S = Mat( image_zero.rows, image_zero.cols, CV_16S );
+		// Mat imgDisparity8U = Mat( image_zero.rows, image_zero.cols, CV_8UC1 );
+
+		// //-- 4. Display it as a CV_8UC1 image
+		// imgDisparity16S.convertTo( imgDisparity8U, CV_8UC1, 255/(maxVal - minVal));
+
+		// namedWindow( "windowDisparity", WINDOW_NORMAL );
+		// imshow( "windowDisparity", imgDisparity8U );
+
+
+		// Mat disp, disp8, g1, g2;
+
+		// //char* method = argv[3];
+		// char* method = "SGBM";
+
+		//img1 = imread(argv[1]);
+		//img2 = imread(argv[2]);
+		// img1 = imread("leftImage.jpg");
+		// img2 = imread("rightImage.jpg");
+
+		// cvtColor(image_zero, g1, CV_BGR2GRAY);
+		// cvtColor(image_one, g2, CV_BGR2GRAY);
+
+		// if (!(strcmp(method, "BM")))
+		// {
+		//     StereoBM sbm;
+		//     sbm.state->SADWindowSize = 9;
+		//     sbm.state->numberOfDisparities = 112;
+		//     sbm.state->preFilterSize = 5;
+		//     sbm.state->preFilterCap = 61;
+		//     sbm.state->minDisparity = -39;
+		//     sbm.state->textureThreshold = 507;
+		//     sbm.state->uniquenessRatio = 0;
+		//     sbm.state->speckleWindowSize = 0;
+		//     sbm.state->speckleRange = 8;
+		//     sbm.state->disp12MaxDiff = 1;
+		//     sbm(g1, g2, disp);
+		// }
+		// else if (!(strcmp(method, "SGBM")))
+		// {
+		//     StereoSGBM sbm;
+		//     sbm.SADWindowSize = 3;
+		//     sbm.numberOfDisparities = 144;
+		//     sbm.preFilterCap = 63;
+		//     sbm.minDisparity = -39;
+		//     sbm.uniquenessRatio = 10;
+		//     sbm.speckleWindowSize = 100;
+		//     sbm.speckleRange = 32;
+		//     sbm.disp12MaxDiff = 1;
+		//     sbm.fullDP = false;
+		//     sbm.P1 = 216;
+		//     sbm.P2 = 864;
+		//     sbm(g1, g2, disp);
+		// }
+
+
+		// normalize(disp, disp8, 0, 255, CV_MINMAX, CV_8U);
+
+		// imshow("left", image_zero);
+		// imshow("right", image_one);
+		// imshow("disp", disp8);
+
+		// waitKey(0);
+
+
+
 
 		IplImage *stacked;
 		struct feature* feat1, * feat2, * feat;
@@ -223,6 +332,9 @@ int main(int argc, char** argv)
 			if ((bboxes[f-START_FRAME][0][2] < XMAX_THRES) && (bboxes[f-START_FRAME][0][3] < YMAX_THRES))
 			{
 
+				int center_x = (bboxes[f-START_FRAME][0][0] + bboxes[f-START_FRAME][0][2]) / 2;
+				int center_y = (bboxes[f-START_FRAME][0][1] + bboxes[f-START_FRAME][0][3]) / 2;
+
 				printf("Found unique car #%d\n", car_count);
 
 				char image_file_processed[100];
@@ -236,6 +348,43 @@ int main(int argc, char** argv)
 				compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 				compression_params.push_back(9);
 
+
+
+				char input[100];
+				sprintf(input, RAW_FRAMES_LOCATION"/frame%05ld.jpeg", f);
+				Mat current_image = imread(input, CV_LOAD_IMAGE_COLOR);
+				cvtColor(current_image, gray, COLOR_BGR2GRAY);
+
+				if( !prevgray.empty() )
+				{
+					calcOpticalFlowFarneback(prevgray, gray, uflow, 0.5, 3, 15, 3, 5, 1.2, 0);
+					cvtColor(prevgray, cflow, COLOR_GRAY2BGR);
+					uflow.copyTo(flow);
+					// drawOptFlowMap(flow, cflow, 16, 1.5, Scalar(0, 255, 0));
+					// imshow("flow", cflow);
+					const Point2f& fxy = flow.at<Point2f>(center_y, center_x);
+					float length = sqrt(pow(fxy.x, 2) + pow(fxy.y, 2));
+					printf("Flow at (%d, %d):\t%f\n\n", center_x, center_y, length);
+					putText(image_processed, to_string(length), cvPoint(center_x, center_y), FONT_HERSHEY_SIMPLEX, 1, cvScalar(0, 0, 255), 2, CV_AA);
+					flux[f - START_FRAME] = length;
+				}
+
+				printf("x:%ld\ty:%f\n", (f-START_FRAME)*10, 10*flux[f-START_FRAME]);
+
+				bbox_heights[f-START_FRAME] = bboxes[f-START_FRAME][0][3] - bboxes[f-START_FRAME][0][1];
+
+				for (unsigned int i = 0; i < f-START_FRAME; i++)
+				{
+					circle(image_processed, Point((i)*10, (720 - 10*flux[i])), 2, Scalar(0, 0, 255), 2, 8);
+					circle(image_processed, Point(bbox_heights[i]*8, (720 - 10*flux[i])), 2, Scalar(255, 0, 0), 2, 8);
+				}
+				// circle(image_processed, Point( 200, 200 ), 32.0, Scalar( 0, 0, 255 ), 1, 8 );
+
+				imshow("flowwww", image_processed);
+				cvWaitKey(1);
+
+				std::swap(prevgray, gray);
+
 				try
 				{
 					imwrite(image_file_processed_new, image_processed, compression_params);
@@ -247,13 +396,48 @@ int main(int argc, char** argv)
 				}
 
 				// Uncomment to wait for keypress when match is detected
-				// cvWaitKey(0);
+				int c = cvWaitKey(30);
+				if (c > 0)
+				{
+					break;
+				}
 			}
 			else if (matched_frames == MINIMUM_FRAMES)
 			{
 				car_count--;
+				char image_file_processed[100];
+				char image_file_processed_new[100];
+				sprintf(image_file_processed, PROCESSED_FRAMES_LOCATION"/frame%05ld.jpeg_p.png", f);
+				sprintf(image_file_processed_new, COUNTED_FRAMES_LOCATION"/frame%05ld.jpeg_p.png", f);
+				vector<int> compression_params;
+				compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+				compression_params.push_back(9);
+				Mat image_processed = imread(image_file_processed, CV_LOAD_IMAGE_COLOR);
+				imwrite(image_file_processed_new, image_processed, compression_params);
 			}
 		}
 	}
+
+	printf("\n-----------------FLUX-----------------\n");
+	for (auto i = flux.begin(); i != flux.end(); ++i)
+		std::cout << *i << '\n';
+
+	printf("\n------------BBOX HEIGHTS--------------\n");
+	for (auto i = bbox_heights.begin(); i != bbox_heights.end(); ++i)
+		std::cout << *i << '\n';
+
+	cvWaitKey(0);
+
+	// namedWindow("plot", WINDOW_NORMAL);
+	// Mat plot(720, 1280, CV_8UC4);
+
+	// for (unsigned int i = 0; i < flux.size(); i++)
+	// {
+	// 	circle(image_processed, Point2f(i*10, flux[i]), 2, Scalar(0, 255, 0));
+	// }
+
+	// imshow("flowwww", image_processed);
+	// cvWaitKey(0);
+
 	return 0;
 }
