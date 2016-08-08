7fe211093c2bdf864b5bdd50bfba4a59ba109d26
Ajay Srivastava <ajaysriv3@gmail.com>
2016-08-08 11:49:35 -0400

    traffic class in counter

--- counter/count.cpp
+++ counter/count.cpp
@@ -28,13 +28,17 @@ extern "C"
 #define XMAX_THRES 640
 #define YMAX_THRES 650
 
-#define START_FRAME 33500
-#define END_FRAME 35298
+#define START_FRAME 2600
+#define END_FRAME 4399
 
-#define BBOX_FILE "../data/1_minute_medium_traffic/car.txt"
-#define RAW_FRAMES_LOCATION "../data/1_minute_medium_traffic/raw_frames"
-#define PROCESSED_FRAMES_LOCATION "../data/1_minute_medium_traffic/processed_frames"
-#define COUNTED_FRAMES_LOCATION "../data/1_minute_medium_traffic/counted_frames"
+#define DISTANCE 0.6 // miles
+#define RECALCULATE_FRAMES 900
+#define LANES 3
+
+#define BBOX_FILE "../data/1_minute_light_traffic/bbox_files/car.txt"
+#define RAW_FRAMES_LOCATION "../data/1_minute_light_traffic/frames/1_raw"
+#define PROCESSED_FRAMES_LOCATION "../data/1_minute_light_traffic/frames/2_yolo"
+#define COUNTED_FRAMES_LOCATION "../data/1_minute_light_traffic/frames/3_processed"
 
 using namespace cv;
 using namespace std;
@@ -52,6 +56,8 @@ int main(int argc, char** argv)
 
 	long int image_index = START_FRAME;
 	long int frame_number = START_FRAME;
+	string traffic_class = "Traffic class: N/A";
+	int prev_car_count = 0;
 
 	// Read bounding box data
 	ifstream bbox_file(BBOX_FILE);
@@ -119,10 +125,11 @@ int main(int argc, char** argv)
 	for (long int f=START_FRAME+1; f<END_FRAME; f++)
 	{
 		char image_file_zero[100];
-		sprintf(image_file_zero, RAW_FRAMES_LOCATION"/frame%5ld.jpeg", f-1);
+		sprintf(image_file_zero, RAW_FRAMES_LOCATION"/frame0%4ld.jpeg", f-1);
 
-		printf("%s\n", image_file_zero);
+		// printf("%s\n", image_file_zero);
 		Mat image_zero = imread(image_file_zero, CV_LOAD_IMAGE_COLOR);
+
 
 		if(bboxes[f-START_FRAME-1].empty())
 		{
@@ -136,10 +143,9 @@ int main(int argc, char** argv)
 		IplImage img1_image = cropped_zero;
 		IplImage* img1 = &img1_image;
 
-
-
 		char image_file_one[100];
-		sprintf(image_file_one, RAW_FRAMES_LOCATION"/raw_frames/frame%5ld.jpeg", f);
+		sprintf(image_file_one, RAW_FRAMES_LOCATION"/frame0%4ld.jpeg", f);
+		// printf("%s\n", image_file_one);
 		Mat image_one = imread(image_file_one, CV_LOAD_IMAGE_COLOR);
 
 		if(bboxes[f-START_FRAME].empty())
@@ -153,6 +159,7 @@ int main(int argc, char** argv)
 		cv::Mat cropped_one = image_one(bound_box_one);
 		IplImage img2_image = cropped_one;
 		IplImage* img2 = &img2_image;
+
 
 		IplImage *stacked;
 		struct feature* feat1, * feat2, * feat;
@@ -198,7 +205,7 @@ int main(int argc, char** argv)
 		// display_big_img(stacked, matches);
 		// cvWaitKey(1);
 
-		printf("%ld\n", f);
+		printf("%ld / %d\n", f, END_FRAME);
 
 		if (m > MINIMUM_FEATURES)
 		{
@@ -223,14 +230,73 @@ int main(int argc, char** argv)
 
 				char image_file_processed[100];
 				char image_file_processed_new[100];
-				sprintf(image_file_processed, PROCESSED_FRAMES_LOCATION"/frame%5ld.jpeg_p.png", f);
-				sprintf(image_file_processed_new, COUNTED_FRAMES_LOCATION"/frame%5ld.jpeg_p.png", f);
+				sprintf(image_file_processed, PROCESSED_FRAMES_LOCATION"/frame0%4ld.jpeg_p.png", f);
+				sprintf(image_file_processed_new, COUNTED_FRAMES_LOCATION"/frame0%4ld.jpeg_p.png", f);
 				Mat image_processed = imread(image_file_processed, CV_LOAD_IMAGE_COLOR);
 				putText(image_processed, to_string(car_count), cvPoint(bboxes[f-START_FRAME][0][2], bboxes[f-START_FRAME][0][3]), FONT_HERSHEY_SIMPLEX, 2, cvScalar(0, 0, 255), 5, CV_AA);
+
+
+
+				string current_car_speed = "Current Car Speed: N/A";
+
+				// % run_nn {flow} {bbox height} {center x} {center y} {depth}
+				char* octave_call[100];
+				// sprintf(octave_call, "octave run_nn.m %f %f %f %f %f > class.txt", , , , )
+				int a = system(octave_call);
+				if (a == -1)
+				{
+					exit(0);
+				}
+
+				ifstream file("class.txt");
+				string file_speed;
+				getline(file, file_speed);
+
+				printf("speed: %s\n", file_speed.c_str());
+
+
+
+				// FILE *lsofFile_p = popen("octave run_nn.m 7 136 225 418 43", "r");
+
+				// if (!lsofFile_p)
+				// {
+				//   return -1;
+				// }
+
+				// char buffer[1024];
+				// char *line_p = fgets(buffer, sizeof(buffer), lsofFile_p);
+				// pclose(lsofFile_p);
+
+				// printf("%s\n", line_p);
+
+				if (((f - START_FRAME) % RECALCULATE_FRAMES) == 0)
+				{
+					double tc_speed = 140 - (car_count - prev_car_count) / DISTANCE / 2;
+					int tc = -1;
+
+					if (tc_speed > 123.33)			{	tc = 1;	}
+					else if (tc_speed > 106.66) 	{	tc = 2;	}
+					else if (tc_speed > 90) 		{	tc = 3;	}
+					else if (tc_speed > 73.33) 		{	tc = 4;	}
+					else if (tc_speed > 56.66) 		{	tc = 5;	}
+					else if (tc_speed > 40) 		{	tc = 6;	}
+					else if (tc_speed > 23.33) 		{	tc = 8;	}
+					else if (tc_speed > 6.66) 		{	tc = 7;	}
+					else if (tc_speed > -10) 		{	tc = 9;	}
+					else 							{	tc = 10;}
+
+					traffic_class = "Traffic class: " + to_string(tc);
+					prev_car_count = car_count;
+				}
+
+				putText(image_processed, traffic_class, cvPoint(10, 30), FONT_HERSHEY_SIMPLEX, 1, cvScalar(0, 0, 255), 2, CV_AA);
+				putText(image_processed, current_car_speed, cvPoint(10, 60), FONT_HERSHEY_SIMPLEX, 1, cvScalar(0, 0, 255), 2, CV_AA);
 
 				vector<int> compression_params;
 				compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 				compression_params.push_back(9);
+
+				imshow("counter", image_processed);
 
 				try
 				{
@@ -242,13 +308,66 @@ int main(int argc, char** argv)
 					return 1;
 				}
 
+
 				// Uncomment to wait for keypress when match is detected
-				// cvWaitKey(0);
+				cvWaitKey(1);
 			}
-			else if (matched_frames == MINIMUM_FRAMES)
+			else
 			{
-				car_count--;
+				if (matched_frames == MINIMUM_FRAMES)
+				{
+					car_count--;
+				}
 			}
+		}
+		else
+		{
+			char image_file_processed[100];
+			char image_file_processed_new[100];
+			sprintf(image_file_processed, PROCESSED_FRAMES_LOCATION"/frame0%4ld.jpeg_p.png", f);
+			sprintf(image_file_processed_new, COUNTED_FRAMES_LOCATION"/frame0%4ld.jpeg_p.png", f);
+			Mat image_processed = imread(image_file_processed, CV_LOAD_IMAGE_COLOR);
+
+			if (((f - START_FRAME) % RECALCULATE_FRAMES) == 0)
+			{
+				double tc_speed = 140 - (car_count - prev_car_count) / DISTANCE / 2;
+				int tc = -1;
+
+				if (tc_speed > 123.33)			{	tc = 1;	}
+				else if (tc_speed > 106.66) 	{	tc = 2;	}
+				else if (tc_speed > 90) 		{	tc = 3;	}
+				else if (tc_speed > 73.33) 		{	tc = 4;	}
+				else if (tc_speed > 56.66) 		{	tc = 5;	}
+				else if (tc_speed > 40) 		{	tc = 6;	}
+				else if (tc_speed > 23.33) 		{	tc = 8;	}
+				else if (tc_speed > 6.66) 		{	tc = 7;	}
+				else if (tc_speed > -10) 		{	tc = 9;	}
+				else 							{	tc = 10;}
+
+				traffic_class = "Traffic class: " + to_string(tc);
+				prev_car_count = car_count;
+			}
+
+			putText(image_processed, traffic_class, cvPoint(10, 30), FONT_HERSHEY_SIMPLEX, 1, cvScalar(0, 0, 255), 2, CV_AA);
+
+			vector<int> compression_params;
+			compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+			compression_params.push_back(9);
+
+			imshow("counter", image_processed);
+
+			try
+			{
+				imwrite(image_file_processed_new, image_processed, compression_params);
+			}
+			catch (runtime_error& ex)
+			{
+				fprintf(stderr, "Exception converting image to PNG format: %s\n", ex.what());
+				return 1;
+			}
+
+			// Uncomment to wait for keypress when match is detected
+			cvWaitKey(1);
 		}
 	}
 	return 0;
---
+++ counter/run_nn.m
@@ -0,0 +1,23 @@
+#! /usr/bin/octave -qf
+
+% depoyable nn by passing in command line args in the following manner:
+% run_nn {flow} {bbox height} {center x} {center y} {depth}
+
+input_layer_size  = 5;
+hidden_layer_size = 20;
+num_labels = 12;
+
+%fprintf('loading prev trained Neural Network Parameters ...\n')
+load('weights.mat');
+
+% Obtain Theta1 and Theta2 back from nn_params
+Theta1 = reshape(weights(1:hidden_layer_size * (input_layer_size + 1)), ...
+                 hidden_layer_size, (input_layer_size + 1));
+
+Theta2 = reshape(weights((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                 num_labels, (hidden_layer_size + 1));
+
+input = argv();
+input = str2double(input);
+pred = predict(Theta1,Theta2,input');
+fprintf('%f\n', pred);
